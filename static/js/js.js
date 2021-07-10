0811e2e3535dbf4668eea28b150193775b605b3e
Tomasz Mostowy <51096065+GaijinZ@users.noreply.github.com>
2021-07-10 16:01:40 +0200

    choosing spells - window popup JS

---
+++ static/js/js.js
@@ -0,0 +1,66 @@
+function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+var csrftoken = getCookie('csrftoken')
+
+$(document).ready(function() {
+        $('#weapon-attack').click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'POST',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    csrfmiddlewaretoken: csrftoken,
+                 },
+                data: $('#hit').serialize(),
+                success: function(response) {
+                    if(response.status == 0){
+                        alert('You have died')
+                        window.location = response.url;
+                    }
+                    else{
+                        $(' #monster-health').html('Health: ' + response.monster_health);
+                        $(' #player-health').html('Health: ' + response.player_health);
+                        $(' #player-exp').html('Experience: ' + response.player_exp);
+                        $(' #player-lvl').html('Level: ' + response.player_lvl);
+                        $(' #monster-type').html(response.monster_type);
+                        location.reload();
+                        if(response.monster_health <= 0) {
+                            alert('You have killed the monster');
+                            $(' #monster-health').html('Health: ' + response.monster_health);
+                        }
+                    }
+                }
+            });
+        });
+    });
+
+const modal = document.querySelector(".spells");
+const trigger = document.querySelector("#trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-spells");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
